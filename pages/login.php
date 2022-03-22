b6593d485e3453e3d415d61b6f47900a327502fd
LULU LE BG <lccoc248@gmail.com>
2022-03-22 18:19:10 +0100

    login modif
    ftrgtrgtrtgtr

--- pages/login.php
+++ pages/login.php
@@ -17,17 +17,7 @@
 
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/style/login.css">
-    <title>Document</title>
-</head>
-<body>
+<link rel="stylesheet" href="/style/login.css">
 <div class="login-box">
     <h2>Login</h2>
     <form method="POST">
@@ -43,5 +33,3 @@
     </form>
 </div>
 
-</body>
-</html>
