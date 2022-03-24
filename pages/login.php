8743fff4b64c1a7f61837374dcfdc491bf7e3f21
LULU LE BG <lccoc248@gmail.com>
2022-03-24 13:45:21 +0100

    login message derreur

--- pages/login.php
+++ pages/login.php
@@ -1,19 +1,20 @@
 <?php
-    if(isset($_POST["accept"])){
-        if(isset($_POST['pseudo']) && isset($_POST['pass'])){
-            if(!empty($_POST['pseudo']) && !empty($_POST['pass'])){
-                if($_POST['pseudo'] == "toto" && $_POST['pass'] == "test"){
-                    $_SESSION["online"] = TRUE;
-                    $_SESSION["coin"] = 200;
-                    header("refresh:0;url=index.php?p=home");
-                }else {
-                    echo "pas les bons id";
-                }
-            }else {
-                echo "pas vide svp";
-            }  
+$id = "";
+if (isset($_POST["accept"])) {
+    if (isset($_POST['pseudo']) && isset($_POST['pass'])) {
+        if (!empty($_POST['pseudo']) && !empty($_POST['pass'])) {
+            if ($_POST['pseudo'] == "toto" && $_POST['pass'] == "test") {
+                $_SESSION["online"] = TRUE;
+                $_SESSION["coin"] = 200;
+                header("refresh:0;url=index.php?p=home");
+            } else {
+                $id = "⚠ pas les bons identifiants ⚠";
+            }
+        } else {
+            $id = "⚠ compléter dans sa totalité le formulaire ⚠";
         }
     }
+}
 
 ?>
 
@@ -21,15 +22,15 @@
 <div class="login-box">
     <h2>Login</h2>
     <form method="POST">
-    <div class="user-box">
-        <input type="text" name="pseudo" required="">
-        <label>Pseudo</label>
-    </div>
-    <div class="user-box">
-        <input type="password" name="pass" required="">
-        <label>Mot de Passe</label>
-    </div>
-    <button type="Submit" name="accept" class="fill">Connexion</button>
+        <div class="user-box">
+            <input type="text" name="pseudo" required="">
+            <label>Pseudo</label>
+        </div>
+        <div class="user-box">
+            <input type="password" name="pass" required="">
+            <label>Mot de Passe</label>
+        </div>
+        <p><?php echo $id; ?></p>
+        <button type="Submit" name="accept" class="fill">Connexion</button>
     </form>
 </div>
-
