f25aa23e09e2fc39785019d0b96e1e9d1cb1fbe6
LULU LE BG <lccoc248@gmail.com>
2022-03-25 14:06:27 +0100

    hover bouton good home et name aussi

--- pages/home.php
+++ pages/home.php
@@ -21,7 +21,7 @@ if (isset($_POST["easy"])) {
           <h2>01</h2>
           <h3>Niveau Facile</h3>
           <img src="../images/easy.png" alt="">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+          <p>Mots courants et relativement courts faciles à deviner</p>
           <div class="container2">
             <div class="center">
               <form action="" method="post">
@@ -45,11 +45,11 @@ if (isset($_POST["easy"])) {
           <h2>02</h2>
           <h3>Niveau Moyen</h3>
           <img src="../images/medium.png" alt="">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+          <p>Des mots souvent utilisés mais plus complexes à deviner.</p>
           <div class="container2">
             <div class="center">
               <form action="" method="post">
-                <button type="submit" class="btn">
+                <button type="submit" name="medium" class="btn">
                   <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
                     <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
                     <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
@@ -69,11 +69,11 @@ if (isset($_POST["easy"])) {
           <h2>03</h2>
           <h3>Niveau Difficile</h3>
           <img src="../images/hard.png" alt="">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+          <p>Mots particulièrement long et complexe à deviner</p>
           <div class="container2">
             <div class="center">
               <form action="" method="post">
-                <button type="submit" class="btn">
+                <button type="submit" name="hard" class="btn">
                   <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
                     <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
                     <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
